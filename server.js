f63e56d6037c17050b8f3f3307a4be94caed694b
phhien203 <phhien203@gmail.com>
2019-08-20 06:48:48 +0700

    Listing 11-2: A shortened "Hello world" http server

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-var http = require('http');
-var server = http.createServer();
-
-server.on('request', function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.write('Hello World!');
-	res.end();
-});
-
-server.listen(4002);
+require('http').createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello world in shortend way');
+}).listen(4002);
