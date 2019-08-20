bfb8b87cdf8e0358d5139c0949cb7d13fb31ca4c
phhien203 <phhien203@gmail.com>
2019-08-20 07:17:50 +0700

    Understanding http.ServerRequest object

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+var util = require('util');
+
 require('http').createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello world in shortend way');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // res.end('Hello world in shortend way');
+  // res.end(req.url);
+  res.end(util.inspect(req.headers));
 }).listen(4002);
